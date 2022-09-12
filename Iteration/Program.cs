71161f306ce694b5cb0e929fb0679aa3c2facc04
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-09-12 14:55:19 +0100

    Added for loop and while loop

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,22 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            //Repeat 20 times
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"1 times {i + 1} is {i + 1}");
+            }
+
+            //Repeat until the user says "stop"
+            bool saidStop = false;
+
+            while(saidStop == false)
+            {
+                Console.WriteLine("Tell me to stop");
+                saidStop = Console.ReadLine().ToLower() == "stop";
+
+
+            }
         }
     }
 }
